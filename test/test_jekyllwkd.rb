a372e52bc3ccb28ba9b1744f21f963de152deec9
Fern Zapata <fern@zapata.cc>
2023-06-07 23:14:23 -0600

    Test: Add a test for the username hashing

--- test/test_jekyllwkd.rb
+++ test/test_jekyllwkd.rb
@@ -6,4 +6,11 @@ class JekyllWKD::Test < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::JekyllWKD::VERSION
   end
+
+  def test_that_it_hashes_user_names
+    require "digest"
+    expected = "apr3aj3jqcf89yd69qd8pkjp3pzawxhx"
+    encoded = ::JekyllWKD::ZBase32.encode Digest::SHA1.digest "example"
+    assert expected == encoded
+  end
 end
