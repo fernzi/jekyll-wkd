2fc768721ce1dcecc05b45fb6a5a1d5b8d347766
Fern Zapata <fern@zapata.cc>
2023-06-19 07:01:27 -0600

    Test: Check the generator does generate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,19 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "jekyll-wkd"
 
 require "minitest/autorun"
+
+def fixture_path name
+  File.expand_path "fixtures/#{name}", __dir__
+end
+
+def fixture_site name, override = {}
+  default = {
+    "destination" => File.expand_path("_site", __dir__),
+    "full_rebuild" => true,
+    "source" => fixture_path(name)
+  }
+  config = Jekyll.configuration Jekyll::Utils.deep_merge_hashes(
+    default, override
+  )
+  Jekyll::Site.new config
+end
