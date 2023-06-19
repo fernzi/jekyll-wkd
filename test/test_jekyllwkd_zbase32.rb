69223507ee337a8187cfbb6bda777646487f79e5
Fern Zapata <fern@zapata.cc>
2023-06-19 08:24:21 -0600

    Test: Make the hashing check easier to expand

--- test/test_jekyllwkd_zbase32.rb
+++ test/test_jekyllwkd_zbase32.rb
@@ -4,12 +4,17 @@ require "test_helper"
 
 module JekyllWKD
   class TestZBase32 < Minitest::Test
+    USERS = {
+      "example" => "apr3aj3jqcf89yd69qd8pkjp3pzawxhx"
+    }
+
     def test_hashing_user_names
       require "digest"
-      example = "example"
-      expects = "apr3aj3jqcf89yd69qd8pkjp3pzawxhx"
-      encodes = ZBase32.encode Digest::SHA1.digest example
-      assert_equal expects, encodes
+
+      USERS.each do |username, expects|
+        encodes = ZBase32.encode Digest::SHA1.digest username
+        assert_equal expects, encodes
+      end
     end
   end
 end
