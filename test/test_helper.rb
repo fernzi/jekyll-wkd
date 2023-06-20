fc25bee1ee572f8eedf74fa47064970c8418679a
Fern Zapata <fern@zapata.cc>
2023-06-19 20:04:53 -0600

    Test: Build test site in a temp dir

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "jekyll-wkd"
 
 require "minitest/autorun"
+require "tmpdir"
 
 def fixture_path name
   File.expand_path "fixtures/#{name}", __dir__
@@ -11,12 +12,11 @@ end
 
 def fixture_site name, override = {}
   default = {
-    "destination" => File.expand_path("_site", __dir__),
+    "destination" => Dir.mktmpdir("jekyll-wkd-test-"),
     "full_rebuild" => true,
     "source" => fixture_path(name)
   }
-  config = Jekyll.configuration Jekyll::Utils.deep_merge_hashes(
-    default, override
-  )
+  config = Jekyll::Utils.deep_merge_hashes default, override
+  config = Jekyll.configuration config
   Jekyll::Site.new config
 end
--- test/test_jekyllwkd_generator.rb
+++ test/test_jekyllwkd_generator.rb
@@ -3,25 +3,31 @@
 require "test_helper"
 
 module JekyllWKD
+  TEST_DOMAIN = "example.com"
+  TEST_KEYS = %w[
+    tkg67ingrxkceoueajuffsyjesntau59
+    wwq7w9d96wfsd4zkytndq84kpkjod3eb
+  ]
+
   class TestGenerator < Minitest::Test
     def setup
       @site = fixture_site "site"
       @site.process
     end
 
+    def teardown
+      FileUtils.remove_dir @site.dest
+    end
+
     def test_generating_a_policy
-      pol = @site.in_dest_dir PGP_PATH, "example.com/policy"
+      pol = @site.in_dest_dir PGP_PATH, TEST_DOMAIN, "policy"
       assert File.exist? pol
     end
 
     def test_exporting_keys
       require "digest"
-      keys = %w[
-        tkg67ingrxkceoueajuffsyjesntau59
-        wwq7w9d96wfsd4zkytndq84kpkjod3eb
-      ]
-      keys.each do |k|
-        key = @site.in_dest_dir PGP_PATH, "example.com/hu", k
+      TEST_KEYS.each do |k|
+        key = @site.in_dest_dir PGP_PATH, TEST_DOMAIN, "hu", k
         assert File.exist? key
       end
     end
